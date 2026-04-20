4f44cb1ab669f4f4e4613c855e727df1e6283412
suitedconnector <tal@trezian.com>
2026-04-20 13:15:47 -0700

    Configure Cloudinary upload provider for Strapi

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -8,4 +8,19 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
